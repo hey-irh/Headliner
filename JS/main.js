776719dd46ae230294b2cf874fb8436fce9d1fc4
hey-irh <isabelholland90@gmail.com>
2021-03-12 12:59:58 +0000

    all news & just headlines saved in single array

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,58 @@
-async function getNews() {
-  const response = await fetch(`http://localhost:5000/news`);
-  console.log(response);
+var theGuardian;
+var bbc;
+var mirror;
+var theTimes;
+var dailyMail;
+var allNews;
+var headLines = [];
+
+// API call per publication due to limitation of free news api
+async function getTheGuardian() {
+  const response = await fetch(`http://localhost:5000/theguardian`);
   const { payload } = await response.json();
   console.log(payload);
+  theGuardian = payload.articles;
+  getBBC();
 }
 
-getNews();
+getTheGuardian();
+
+async function getBBC() {
+  const response = await fetch(`http://localhost:5000/bbc`);
+  const { payload } = await response.json();
+  console.log(payload);
+  bbc = payload.articles;
+  getMirror();
+}
+
+async function getMirror() {
+  const response = await fetch(`http://localhost:5000/mirror`);
+  const { payload } = await response.json();
+  console.log(payload);
+  mirror = payload.articles;
+  getTheTimes();
+}
+
+async function getTheTimes() {
+  const response = await fetch(`http://localhost:5000/thetimes`);
+  const { payload } = await response.json();
+  console.log(payload);
+  theTimes = payload.articles;
+  getDailyMail();
+}
+
+async function getDailyMail() {
+  const response = await fetch(`http://localhost:5000/dailymail`);
+  const { payload } = await response.json();
+  console.log(payload);
+  dailyMail = payload.articles;
+  concat();
+}
+
+function concat() {
+  if (dailyMail !== 'undefined') {
+    allNews = theGuardian.concat(bbc, mirror, theTimes, dailyMail);
+    allNews.forEach((allNews) => headLines.push(allNews.title));
+    console.log(headLines);
+  } else concat();
+}
--- app.js
+++ app.js
@@ -14,12 +14,68 @@ app.listen(port, () => {
 
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('2c7880703dde46bba0f3a7f808d7e380');
+var today = new Date();
+var todayformat = today.toISOString().substring(0, 10);
 
-app.get('/news', async function (req, res) {
+app.get('/theguardian', async function (req, res) {
   const news = await newsapi.v2.everything({
     language: 'en',
-    domains:
-      'bbc.co.uk, theguardian.com, mirror.co.uk, thetimes.co.uk, dailymail.co.uk',
+    from: todayformat,
+    to: todayformat,
+    sortBy: 'popularity',
+    domains: 'theguardian.com',
+  });
+  console.log(news);
+  res.json({ success: true, payload: news });
+  console.log(news);
+});
+
+app.get('/bbc', async function (req, res) {
+  const news = await newsapi.v2.everything({
+    language: 'en',
+    from: todayformat,
+    to: todayformat,
+    sortBy: 'popularity',
+    domains: 'bbc.co.uk',
+  });
+  console.log(news);
+  res.json({ success: true, payload: news });
+  console.log(news);
+});
+
+app.get('/mirror', async function (req, res) {
+  const news = await newsapi.v2.everything({
+    language: 'en',
+    from: todayformat,
+    to: todayformat,
+    sortBy: 'popularity',
+    domains: 'mirror.co.uk',
+  });
+  console.log(news);
+  res.json({ success: true, payload: news });
+  console.log(news);
+});
+
+app.get('/thetimes', async function (req, res) {
+  const news = await newsapi.v2.everything({
+    language: 'en',
+    from: todayformat,
+    to: todayformat,
+    sortBy: 'popularity',
+    domains: 'thetimes.co.uk',
+  });
+  console.log(news);
+  res.json({ success: true, payload: news });
+  console.log(news);
+});
+
+app.get('/dailymail', async function (req, res) {
+  const news = await newsapi.v2.everything({
+    language: 'en',
+    from: todayformat,
+    to: todayformat,
+    sortBy: 'popularity',
+    domains: 'dailymail.co.uk',
   });
   console.log(news);
   res.json({ success: true, payload: news });
